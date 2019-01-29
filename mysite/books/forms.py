22e94ac7ee0bab568b307a0fa0cd0b2a372e8d01
Shubham Bansal <shubhambansal17@hotmail.com>
2019-01-30 02:04:40 +0530

    Removed the issue for the latest bootstrap and django

--- mysite/books/forms.py
+++ mysite/books/forms.py
@@ -4,6 +4,11 @@ from .models import Book
 
 
 class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ('title', 'publication_date', 'author', 'price', 'pages', 'book_type', )
+	def __init__(self, *args, **kwargs):
+		super(BookForm, self).__init__(*args, **kwargs)
+		for field_name, field in self.fields.items():
+			field.widget.attrs['class'] = 'form-control'
+
+	class Meta:
+		model = Book
+		fields = ('title', 'publication_date', 'author', 'price', 'pages', 'book_type',)
